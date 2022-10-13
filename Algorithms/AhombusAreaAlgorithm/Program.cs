4fadcf421296bc24c56ffe99f2e79d05a92f1a60
Sayed ELGhanam <sayed082961@feng.bu.edu.eg>
2022-10-13 12:49:41 +0200

    create Circle and rhombus areas algorithms

---
+++ Algorithms/AhombusAreaAlgorithm/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(RhombusArea(6,4));
+Console.WriteLine(RhombusArea(9,6));
+Console.WriteLine(RhombusArea(15,10));
+
+/// <summary>
+/// length (l),
+/// heigth (h)
+/// </summary>
+double RhombusArea(double l, double h)
+{
+    var a = l * h;
+    return a;
+}
---
+++ Algorithms/CircleAreaAlgorithm/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(CircleArea(10));
+Console.WriteLine(CircleArea(20));
+Console.WriteLine(CircleArea(30));
+
+double CircleArea(double r)
+{
+    return Math.PI * r * r;
+}
